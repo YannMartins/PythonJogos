8a76a220e1254d4c50c57bd87597b5cf4a502ca4
Yann Martins <102706324+YannMartins@users.noreply.github.com>
2023-05-12 17:09:41 -0300

    Adicionando arquivo do jogo de adivinhação

---
+++ jogoDeAdivinhacao.py
@@ -0,0 +1,37 @@
+"""
+UNIESP Centro Universitário
+Curso: Análise e Desenvolvimento de Sistemas
+Projeto Integrador I 
+Discente: Yann Martins de Sousa
+Matrícula: 2023111590065
+"""
+
+from random import randint
+random = randint(1,100)
+
+print("")
+print("Boas-vindas! Vamos jogar um jogo de adivinhação?")
+print("Adivinhe qual o número escolhido pelo computador entre 1 e 100 em 10 tentativas.")
+print("")
+
+tentativa = 0
+
+while tentativa < 10:
+    palpite = int(input('Insira o seu palpite: '))
+    
+    if palpite > random:
+        print("Você errou! Dica: seu palpite foi alto.")
+        print("")
+        tentativa += 1
+    elif palpite < random:
+        print("Você errou! Dica: seu palpite foi baixo.")
+        print("")
+        tentativa += 1
+    else:
+        print("")
+        print("Parabéns, você acertou em cheio!")
+        print("")
+        break;
+else:
+    print("Você atingiu o número máximo de tentativas! O palpite certo era: {}." .format (random))
+    print("")
